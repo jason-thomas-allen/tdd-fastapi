c7d7067b37f2ad90bda79df4237be3079f5304f5
Jason Allen <jason.thomas.allen@protonmail.com>
2021-07-04 12:01:28 +0800

    Restful routes

--- project/app/main.py
+++ project/app/main.py
@@ -2,8 +2,7 @@ from os import environ
 import logging
 from fastapi import FastAPI
 
-from app.api import ping
-from app.config import get_settings, Settings
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
@@ -12,6 +11,8 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
@@ -22,6 +23,7 @@ app = create_application()
 @app.on_event("startup")
 async def startup_event():
     log.info("Starting up...")
+    init_db(app)
 
 
 @app.on_event("shutdown")
